ed2630d8a2c1dbdbfa98f6e9ec43f4f460d60529
RYE <robbyeadam@gmail.com>
2024-11-26 09:59:10 +0700

    feature: implemented remove item from basket feature

---
+++ src/Services/Basket/Basket.Api/Features/DeleteItemsFromBasket/DeleteItemsFromBasketEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Features.DeleteItemsFromBasket;
+
+public record DeleteItemsFromBasketRequest(
+    IEnumerable<Guid> BookIds);
+
+public record DeleteItemsFromBasketResponse(
+    bool Success);
+public class DeleteItemsFromBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/baskets/{username}/items", async (string username,[FromBody] DeleteItemsFromBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<DeleteItemsFromBasketCommand>();
+            command = command with { Username = username };
+
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteItemsFromBasketResponse>();
+            return Results.Ok(response);
+        });
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Features/DeleteItemsFromBasket/DeleteItemsFromBasketHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Basket.Api.Features.DeleteItemsFromBasket;
+
+public record DeleteItemsFromBasketCommand(string Username, IEnumerable<Guid> BookIds) : ICommand<DeleteItemsFromBasketResult>;
+public record DeleteItemsFromBasketResult(bool Success);
+
+public class DeleteItemsFromBasketCommandHandler(
+    ApplicationDbContext context) : ICommandHandler<DeleteItemsFromBasketCommand, DeleteItemsFromBasketResult>
+{
+    public async Task<DeleteItemsFromBasketResult> Handle(DeleteItemsFromBasketCommand command, CancellationToken cancellationToken)
+    {
+        var basket = await context.ShoppingCarts
+            .Include(x => x.Items)
+            .FirstOrDefaultAsync(x => x.Username == command.Username);
+
+        if (basket is null)
+            return new DeleteItemsFromBasketResult(false);
+
+        var bookIdsToRemove = command.BookIds.ToHashSet();
+
+        var itemsToRemove = basket.Items
+            .Where(item => bookIdsToRemove.Contains(item.BookId))
+            .ToList();
+
+        if(itemsToRemove.Count == 0)
+            return new DeleteItemsFromBasketResult(false);
+
+
+        foreach (var item in itemsToRemove)
+        {
+            basket.Items.Remove(item);
+        }  
+
+        await context.SaveChangesAsync(cancellationToken);
+        return new DeleteItemsFromBasketResult(true);
+    }
+}
